1cff8521449657836480fb8bec7d0adbdf538364
Zirconium419122 <rasmus@liaskar.net>
2025-01-04 19:29:17 +0100

    Refactor perft test by moving code into the Perft struct, eliminating the need to construct a transposition table

--- tests/perft.rs
+++ tests/perft.rs
@@ -1,34 +1,42 @@
 use chess_frame::board::Board;
 
-type TranspositionTable = std::collections::HashMap<u64, (usize, usize)>;
+struct Perft(std::collections::HashMap<u64, (usize, usize)>);
 
-fn perft(board: &Board, transposition_table: &mut TranspositionTable, depth: usize, divide: bool) -> usize {
-    if depth == 0 {
-        return 1;
+impl Perft {
+    pub fn run(board: &Board, depth: usize, divide: bool) -> usize {
+        let mut perft = Perft(std::collections::HashMap::new());
+
+        perft.perft(&board, depth, divide)
     }
 
-    if let Some((perft_result, transposition_depth)) = transposition_table.get(&board.get_hash()) {
-        if *transposition_depth == depth {
-            return *perft_result;
+    fn perft(&mut self, board: &Board, depth: usize, divide: bool) -> usize {
+        if depth == 0 {
+            return 1;
         }
-    }
-
-    let mut count = 0;
-
-    for mv in board.generate_moves_vec() {
-        if let Ok(ref board) = board.make_move_new(&mv) {
-            let perft_results = perft(board, transposition_table, depth - 1, false);
-            count += perft_results;
-
-            if divide {
-                println!("{}: {}", mv, perft_results);
+    
+        if let Some((perft_result, transposition_depth)) = self.0.get(&board.get_hash()) {
+            if *transposition_depth == depth {
+                return *perft_result;
             }
         }
+    
+        let mut count = 0;
+    
+        for mv in board.generate_moves_vec() {
+            if let Ok(ref board) = board.make_move_new(&mv) {
+                let perft_results = self.perft(board, depth - 1, false);
+                count += perft_results;
+    
+                if divide {
+                    println!("{}: {}", mv, perft_results);
+                }
+            }
+        }
+    
+        self.0.insert(board.get_hash(), (count, depth));
+    
+        count
     }
-
-    transposition_table.insert(board.get_hash(), (count, depth));
-
-    count
 }
 
 #[test]
@@ -36,9 +44,7 @@ fn test_perft_depth_1() {
     let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     let board = Board::from_fen(fen);
 
-    let transposition_table = &mut TranspositionTable::new();
-
-    assert_eq!(perft(&board, transposition_table, 1, false), 20);
+    assert_eq!(Perft::run(&board, 1, false), 20);
 }
 
 #[test]
@@ -46,9 +52,7 @@ fn test_perft_depth_2() {
     let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     let board = Board::from_fen(fen);
 
-    let transposition_table = &mut TranspositionTable::new();
-
-    assert_eq!(perft(&board, transposition_table, 2, false), 400);
+    assert_eq!(Perft::run(&board, 2, false), 400);
 }
 
 #[test]
@@ -56,9 +60,7 @@ fn test_perft_depth_3() {
     let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     let board = Board::from_fen(fen);
 
-    let transposition_table = &mut TranspositionTable::new();
-
-    assert_eq!(perft(&board, transposition_table, 3, true), 8902);
+    assert_eq!(Perft::run(&board, 3, false), 8902);
 }
 
 #[test]
@@ -66,9 +68,7 @@ fn test_perft_depth_4() {
     let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     let board = Board::from_fen(fen);
 
-    let transposition_table = &mut TranspositionTable::new();
-
-    assert_eq!(perft(&board, transposition_table, 4, false), 197281);
+    assert_eq!(Perft::run(&board, 4, false), 197281);
 }
 
 #[test]
@@ -76,9 +76,7 @@ fn test_perft_depth_5() {
     let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     let board = Board::from_fen(fen);
 
-    let transposition_table = &mut TranspositionTable::new();
-
-    assert_eq!(perft(&board, transposition_table, 5, false), 4865609);
+    assert_eq!(Perft::run(&board, 5, false), 4865609);
 }
 
 // These test should only be run in release mode as during debug profile it's much slower
@@ -88,9 +86,7 @@ fn test_perft_depth_6() {
     let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     let board = Board::from_fen(fen);
 
-    let transposition_table = &mut TranspositionTable::new();
-
-    assert_eq!(perft(&board, transposition_table, 6, false), 119060324);
+    assert_eq!(Perft::run(&board, 6, false), 119060324);
 }
 
 #[test]
@@ -99,7 +95,5 @@ fn test_perft_depth_7() {
     let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     let board = Board::from_fen(fen);
 
-    let transposition_table = &mut TranspositionTable::new();
-
-    assert_eq!(perft(&board, transposition_table, 7, false), 3195901860);
+    assert_eq!(Perft::run(&board, 7, false), 3195901860);
 }
