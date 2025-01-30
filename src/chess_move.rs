8fbe7186964a1d13840acb014b22e2398b859f03
Zirconium419122 <rasmus@liaskar.net>
2025-01-30 19:56:35 +0100

    Add documentation for all ChessMove constructors and methods; move new_promotion to below the new constructor

--- src/chess_move.rs
+++ src/chess_move.rs
@@ -44,6 +44,21 @@ impl fmt::Display for ChessMove {
 }
 
 impl ChessMove {
+    /// Create a new `ChessMove` given a `from` and `to` square.
+    /// 
+    /// # Parameters
+    /// - `from` a [`Square`] representing the starting square of the move.
+    /// - `to` a [`Square`] representing the ending square of the move.
+    /// 
+    /// # Example
+    /// ```
+    /// use chess_frame::{board::Board, chess_move::ChessMove, square::Square};
+    ///
+    /// let board = Board::default();
+    /// let mv = ChessMove::new(Square::E2, Square::E4);
+    ///
+    /// assert_eq!(board.make_move_new(&mv), Ok(Board::from_fen("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1")));
+    /// ```
     pub fn new(from: Square, to: Square) -> ChessMove {
         ChessMove {
             from,
@@ -52,20 +67,63 @@ impl ChessMove {
         }
     }
 
-    pub fn get_move(&self) -> (Square, Square) {
-        (self.from, self.to)
-    }
-
-    pub fn promotion(&self) -> Option<Piece> {
-        self.promotion
-    }
-
+    /// Create a new `ChessMove` given a `from` and `to` square and a `promotion` piece.
+    /// 
+    /// # Parameters
+    /// - `from` a [`Square`] representing the starting square of the move.
+    /// - `to` a [`Square`] representing the ending square of the move.
+    /// - `promotion` a [`Piece`] representing the piece to promote to.
+    /// 
+    /// # Example
+    /// ```
+    /// use chess_frame::{board::Board, chess_move::ChessMove, piece::Piece, square::Square};
+    ///
+    /// let fen = "7r/1Pk5/8/K7/8/8/8/1R6 w - - 0 1";
+    /// let board = Board::from_fen(fen);
+    /// let mv = ChessMove::new_promotion(Square::B7, Square::B8, Piece::Queen);
+    ///
+    /// assert_eq!(board.make_move_new(&mv), Ok(Board::from_fen("1Q5r/2k5/8/K7/8/8/8/1R6 b - - 0 1")));
+    /// ```
     pub fn new_promotion(from: Square, to: Square, promotion: Piece) -> ChessMove {
         ChessMove {
             from,
             to,
             promotion: Some(promotion),
         }
+    }
+
+    /// Get the `from` and `to` squares of the move.
+    /// 
+    /// # Returns
+    /// - `(Square, Square)` a tuple containing the `from` and `to` squares of the move.
+    /// 
+    /// # Example
+    /// ```
+    /// use chess_frame::{chess_move::ChessMove, square::Square};
+    ///
+    /// let mv = ChessMove::new(Square::E2, Square::E4);
+    ///
+    /// assert_eq!(mv.get_move(), (Square::E2, Square::E4));
+    /// ```
+    pub fn get_move(&self) -> (Square, Square) {
+        (self.from, self.to)
+    }
+
+    /// Get the promotion piece of the move.
+    /// 
+    /// # Returns
+    /// - [`Option<Piece>`] an optional [`Piece`] representing the promotion piece of the move.
+    /// 
+    /// # Example
+    /// ```
+    /// use chess_frame::{chess_move::ChessMove, piece::Piece, square::Square};
+    ///
+    /// let mv = ChessMove::new_promotion(Square::B7, Square::B8, Piece::Queen);
+    ///
+    /// assert_eq!(mv.promotion(), Some(Piece::Queen));
+    /// ```
+    pub fn promotion(&self) -> Option<Piece> {
+        self.promotion
     }
 }
 
