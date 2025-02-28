e5c2a51c56e578eacab4b0a37870f7fd9c048374
Zirconium419122 <rasmus@liaskar.net>
2025-02-28 18:38:28 +0100

    Add documentation to TranspositionTable, document all methods with a sentence

--- src/transpositiontable.rs
+++ src/transpositiontable.rs
@@ -7,12 +7,14 @@ pub struct Entry<T> {
     pub depth: u8,
 }
 
+#[derive(Debug, Clone, PartialEq, PartialOrd, Hash, Default)]
 pub struct TranspositionTable<T> {
     table: Vec<Option<Entry<T>>>,
     max_entries: usize,
 }
 
 impl<T: Debug + Clone + Copy + PartialEq + PartialOrd + Hash + Default> TranspositionTable<T> {
+    /// Creat a new [`TranspositionTable`] with capacity for the next power of two given the number of entries.
     pub fn with_capacity(num_entries: usize) -> TranspositionTable<T> {
         let size = num_entries.next_power_of_two();
 
@@ -22,6 +24,7 @@ impl<T: Debug + Clone + Copy + PartialEq + PartialOrd + Hash + Default> Transpos
         }
     }
 
+    /// Create a new [`TranspositionTable`] with the given the size in megabytes.
     pub fn with_size_mb(size_mb: usize) -> TranspositionTable<T> {
         let entry_size = std::mem::size_of::<Entry<T>>();
         let num_entries = (size_mb * 1024 * 1024) / entry_size;
@@ -33,6 +36,7 @@ impl<T: Debug + Clone + Copy + PartialEq + PartialOrd + Hash + Default> Transpos
         (zobrist as usize) & (self.max_entries - 1)
     }
 
+    /// Store the given value in the [`TranspositionTable`] provided `zobrist`, `depth` and the `value`.
     pub fn store(&mut self, zobrist: u64, value: T, depth: u8) {
         let index = self.index(zobrist);
 
@@ -48,11 +52,13 @@ impl<T: Debug + Clone + Copy + PartialEq + PartialOrd + Hash + Default> Transpos
         }
     }
 
+    /// Get the value stored in the [`TranspositionTable`] provided `zobrist`.
     pub fn get(&self, zobrist: u64) -> Option<&Entry<T>> {
         let index = self.index(zobrist);
         self.table[index].as_ref().filter(|e| e.zobrist == zobrist)
     }
 
+    /// Clear the [`TranspositionTable`].
     pub fn clear(&mut self) {
         self.table.fill(None);
     }
